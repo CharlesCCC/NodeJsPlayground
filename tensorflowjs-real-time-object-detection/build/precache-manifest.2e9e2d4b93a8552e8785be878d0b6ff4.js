95390f3c1e76d0ff7e6d661213b355e3dd5e5121
COCAI1 <Charles.Cai@emerson.com>
2019-03-08 16:33:31 -0500

    update

---
+++ tensorflowjs-real-time-object-detection/build/precache-manifest.2e9e2d4b93a8552e8785be878d0b6ff4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d6af01f7b135995cf46b",
+    "url": "/static/js/main.d6af01f7.chunk.js"
+  },
+  {
+    "revision": "35b8f87e9229af44127c",
+    "url": "/static/js/2.35b8f87e.chunk.js"
+  },
+  {
+    "revision": "d6af01f7b135995cf46b",
+    "url": "/static/css/main.5aad3e25.chunk.css"
+  },
+  {
+    "revision": "432b5b7ce1166ce56bb6f3877f6bcedf",
+    "url": "/index.html"
+  }
+];
